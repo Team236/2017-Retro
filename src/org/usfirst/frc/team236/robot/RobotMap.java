2749f0c2661159d81b4cabbeb2aaf2c18a51578f
Sam Fuchs <samcfuchs@gmail.com>
2017-11-16 16:39:15 -0500

    Add solenoid channels for garage door

--- src/org/usfirst/frc/team236/robot/RobotMap.java
+++ src/org/usfirst/frc/team236/robot/RobotMap.java
@@ -10,4 +10,12 @@ public class RobotMap {
 	public static class Climber {
 		public static final int PWM_CLIMBER = 7;
 	}
+	
+	public static class Garage {
+		public static final int SOL_GRASPER_FORWARD = 2;
+		public static final int SOL_GRASPER_REVERSE = 3;
+
+		public static final int SOL_VERTICAL_FORWARD = 4;
+		public static final int SOL_VERTICAL_REVERSE = 5;
+	}
 }
