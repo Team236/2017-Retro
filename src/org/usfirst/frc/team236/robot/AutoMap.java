91bbe3413dec21acee6bb0b830e0727736ebea02
Sam Fuchs <samcfuchs@gmail.com>
2017-12-12 21:49:00 -0500

    Add AutoMap

---
+++ src/org/usfirst/frc/team236/robot/AutoMap.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team236.robot;
+
+public final class AutoMap {
+	// Straight gear delivery
+	public static final double straightGear = 82;
+	
+	public static final double turn1Margin = 3;
+	public static final double turn2Margin = 5;
+
+	// Right gear delivery
+	public static final double rightGearLeg1 = 91;
+	public static final double rightGearTurnDegrees = -60;
+	public static final double rightGearLeg2 = 30;
+
+	// Left gear delivery
+	public static final double leftGearLeg1 = 91;
+	public static final double leftGearTurnDegrees = 60;
+	public static final double leftGearLeg2 = 30;
+}
